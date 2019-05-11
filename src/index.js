b35b0b59e0289ec8633e63c6f8a4bab9e59de253
Florian Goebel <florian.goebel@outlook.de>
2019-05-11 18:42:34 +0200

    Added websocket service/store.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { WebsocketStore } from './services/websocketStore';
+import {User} from './models/user';
+
+
+const websocketStore = new WebsocketStore();
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
